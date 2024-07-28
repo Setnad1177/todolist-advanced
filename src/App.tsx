98f21051bf0e0109a692095e0c687720ab2e7d96
Andrew <abrakadabro17@gmail.com>
2024-07-28 17:42:00 +0100

    todolist component (2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
-import {Todolist} from "./Todolist";
+import {TaskType, Todolist} from "./Todolist";
+
+let tasks1: Array<TaskType> = [
+    { id: 1, title: "CSS", isDone: true },
+    { id: 2, title: "CSS", isDone: true },
+    { id: 3, title: "CSS", isDone: false },
+]
+
+let tasks2: Array<TaskType> = [
+    { id: 1, title: "Terminator", isDone: true },
+    { id: 2, title: "XXX", isDone: true },
+    { id: 3, title: "Gentlemen of fortune", isDone: false },
+]
 
 function App() {
     return (
         <div className="App">
-            <Todolist/>
-            <Todolist/>
-            <Todolist/>
+            <Todolist title="What to learn" tasks={tasks1}/>
+            <Todolist title="Movies" tasks={tasks2}/>
         </div>
     );
 }
