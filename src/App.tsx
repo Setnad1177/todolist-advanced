490294767730d2bc41f764b05366228770fc1cb2
Andrew <abrakadabro17@gmail.com>
2024-07-31 22:30:00 +0100

    todolist component, use state

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,36 @@ import './App.css';
 import {TaskType, Todolist} from "./Todolist";
 
 let tasks1: Array<TaskType> = [
-    { id: 1, title: "HTML&CSS", isDone: true },
-    { id: 2, title: "JS", isDone: true },
-    { id: 3, title: "ReactJS", isDone: false },
-    { id: 4, title: "Redux", isDone: false },
+    {id: 1, title: "HTML&CSS", isDone: true},
+    {id: 2, title: "JS", isDone: true},
+    {id: 3, title: "ReactJS", isDone: false},
+    {id: 4, title: "Redux", isDone: false},
 ]
+
+useState(tasks)
+
 
 function App() {
 
     let tasks: Array<TaskType> = [
-        { id: 1, title: "HTML&CSS", isDone: true },
-        { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false },
-        { id: 4, title: "Redux", isDone: false },
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Redux", isDone: false},
     ]
+
+    function removeTask(id: number) {
+        tasks = tasks.filter((t=>t.id !== id))
+    }
+
 
     return (
         <div className="App">
-            <Todolist title="What to learn" tasks={tasks1}/>
-            <Todolist title="Movies" tasks={tasks}/>
-        </div>
+            <Todolist title="What to learn"
+                      tasks={tasks1}
+                      removeTask={removeTask}
+            />
+           </div>
     );
 }
 
