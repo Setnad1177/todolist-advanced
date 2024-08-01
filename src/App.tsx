f16bbe0af03b6db51b54fd48502b48527e35961b
Andrew <abrakadabro17@gmail.com>
2024-08-01 13:58:00 +0100

    todolist component, use state delete task

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {TaskType, Todolist} from "./Todolist";
-
-let tasks1: Array<TaskType> = [
-    {id: 1, title: "HTML&CSS", isDone: true},
-    {id: 2, title: "JS", isDone: true},
-    {id: 3, title: "ReactJS", isDone: false},
-    {id: 4, title: "Redux", isDone: false},
-]
-
-useState(tasks)
 
 
 function App() {
 
-    let tasks: Array<TaskType> = [
+    let initTasks = [
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
         {id: 4, title: "Redux", isDone: false},
     ]
 
+    let [tasks, setTasks] = useState(initTasks);
+
     function removeTask(id: number) {
-        tasks = tasks.filter((t=>t.id !== id))
+        let filteredTasks = tasks.filter((t=>t.id !== id))
+        setTasks(filteredTasks);
     }
 
 
     return (
         <div className="App">
             <Todolist title="What to learn"
-                      tasks={tasks1}
+                      tasks={tasks}
                       removeTask={removeTask}
             />
            </div>
