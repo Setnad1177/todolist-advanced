98edfee010b148a1d22dc69eefad3eedca76771a
Andrew <abrakadabro17@gmail.com>
2024-10-08 12:40:00 +0100

    From begining another way

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,26 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import {TaskType, Todolist} from "./Todolist";
-
-
-export type FilterValuesType = "all" | "completed" | "active";
 
 function App() {
-//comment for resting
-    let [tasks, setTasks] = useState<Array<TaskType>>([
-        {id: 1, title: "HTML&CSS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "ReactJS", isDone: false},
-        {id: 4, title: "Redux", isDone: false},
-    ]);
-
-    let [filter, setFilter] = useState<FilterValuesType>("active")
-
-    function removeTask(id: number) {
-        let filteredTasks = tasks.filter((t => t.id !== id))
-        setTasks(filteredTasks);
-    }
-
-    // function changeFilter(value: FilterValuesType) {
-    //
-    // }
-
-    let tasksForTodolist = tasks;
-
-    if (filter === "completed") {
-        tasksForTodolist = tasks.filter(t => t.isDone === true);
-    }
-
-    if (filter === "active") {
-        tasksForTodolist = tasks.filter(t => t.isDone === false);
-    }
-
-
     return (
         <div className="App">
-            <Todolist title="What to learn"
-                      tasks={tasksForTodolist}
-                      removeTask={removeTask}
-            />
+            <div>
+                <h3>What to learn</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
         </div>
     );
 }
