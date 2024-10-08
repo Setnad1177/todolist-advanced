deea94170b38b6cc95bc3276bd0143024e1f71ee
Andrew <abrakadabro17@gmail.com>
2024-10-08 22:58:00 +0100

    Props, types (2)

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from "react"
+import {TaskType} from "./App";
 
-export const Todolist = (props: any) => {
+type PropsType = {
+    title: string
+    subTitle?: string
+    description?: string
+    tasks: TaskType[]
+}
+
+export const Todolist = ({title, tasks}: PropsType) => {
 
 
     return (
         <div>
-            <h3>{props.title}</h3>
+
+            <h3>{title}</h3>
+            {/*<h4>{subTitle}</h4>*/}
+            {/*<p>{description}</p>*/}
+
+            <div>{tasks.map(t => t.title)}</div>
+
             <div>
                 <input/>
                 <button>+</button>
             </div>
+
             <ul>
                 <li>
                     <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
@@ -21,11 +36,13 @@ export const Todolist = (props: any) => {
                     <input type="checkbox" checked={false}/> <span>React</span>
                 </li>
             </ul>
+
             <div>
                 <button>All</button>
                 <button>Active</button>
                 <button>Completed</button>
             </div>
+
         </div>
     )
 }
