995406ee9126f577341c2019865d4cd3f557ea42
Andrew <abrakadabro17@gmail.com>
2024-10-09 10:56:00 +0100

    Array .map

--- src/App.tsx
+++ src/App.tsx
@@ -13,18 +13,21 @@ export function App() {
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Redux", isDone: false},
+        {id: 5, title: "Typescript", isDone: false},
+        {id: 6, title: "RTK query", isDone: false},
     ]
 
-    const tasks2 = [
+    const tasks2: Array<TaskType> = [
         {id: 1, title: "Hello world", isDone: true},
         {id: 2, title: "I am Happy", isDone: false},
-        {id: 3, title: "Yo", isDone: "false"},
+        {id: 3, title: "Yo", isDone: false},
     ]
 
     return (
         <div className="App">
-            <Todolist title="What to learn" tasks={tasks1}/>
-            <Todolist title="Songs" tasks={tasks1}/>
+            <Todolist title="What to learn" tasks={tasks1} date={"01.01.2011"}/>
+            <Todolist title="Songs" tasks={tasks2}/>
         </div>
     );
 }
