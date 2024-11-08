1336a8aa07de0bd5fb488937f0a22b7138a088da
Andrew <abrakadabro17@gmail.com>
2024-11-08 10:42:46 +0100

    Task title edit mode on double click

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,7 +1,25 @@
+import {useState} from "react";
+import {Simulate} from "react-dom/test-utils";
+import input = Simulate.input;
+
 type PropsType = {
     value: string
 }
 
 export const EditableSpan = ({ value }: PropsType) => {
-    return <span>{value}</span>
+    const [editMode, setEditMode] = useState(false)
+
+    const activateEditModeHandler = () => {
+        setEditMode(true)
+    }
+
+    return (
+        <>
+            {editMode ? (
+                <input value={value} autoFocus />
+            ) : (
+                <span onDoubleClick={activateEditModeHandler}>{value}</span>
+            )}
+        </>
+    )
 }
