6988b21e27cdb31bd563fa31cae2cb4fea6c873f
Andrew <abrakadabro17@gmail.com>
2024-11-09 15:56:37 +0100

    Controlled input (able to type new text while editing task title)

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {ChangeEvent, useState} from "react";
 import {Simulate} from "react-dom/test-utils";
 import input = Simulate.input;
 
@@ -8,6 +8,7 @@ type PropsType = {
 
 export const EditableSpan = ({ value }: PropsType) => {
     const [editMode, setEditMode] = useState(false)
+    const [title, setTitle] = useState(value)
 
     const activateEditModeHandler = () => {
         setEditMode(true)
@@ -17,10 +18,17 @@ export const EditableSpan = ({ value }: PropsType) => {
         setEditMode(false)
     }
 
+    const changeTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+    }
+
     return (
         <>
             {editMode ? (
-                <input value={value} onBlur={deactivateEditModeHandler} autoFocus />
+                <input value={title}
+                       onChange={changeTitleHandler}
+                       onBlur={deactivateEditModeHandler}
+                       autoFocus />
             ) : (
                 <span onDoubleClick={activateEditModeHandler}>{value}</span>
             )}
